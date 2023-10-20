e201a7be0ad00a7f1cc8bdf995ef0aca603e0b21
Firas_El-Ezzi <firaselezzi1@gmail.com>
2023-10-20 17:30:15 -0400

    WILDCARD

--- WildCard.java
+++ WildCard.java
@@ -1,12 +1,47 @@
+import java.util.Scanner;
+
+
+
 public class WildCard extends Card {
 
 
     public WildCard(){
+
         super();
     }
 
+
     @Override
+
     public void playCard(UnoGame game){
+
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        String input = scanner.nextLine();
+        scanner.close();
+
+        System.out.println("Enter A Colour of your choice: ");
+        String upInput = input.toUpperCase();
+
+
+
+
+        if (upInput == "BLUE"){
+            this.setColour(Colour.BLUE);
+
+        }
+        else if(upInput== "RED"){
+            this.setColour(Colour.RED);
+
+        }
+        else if(upInput == "YELLOW"){
+            this.setColour(Colour.YELLOW);
+
+        }
+        else if(upInput == "GREEN"){
+            this.setColour(Colour.GREEN);
+        }
+        System.out.println(game.getCurrentPlayer()+" has changed colour to: " +upInput+" !");
+
 
     }
 
